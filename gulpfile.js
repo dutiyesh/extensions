15c9271b8ac2c002dbfd0c46b29aaad8b4d9d934
Dutiyesh Salunkhe <dutiyesh@gmail.com>
2022-09-13 19:52:49 +0530

    Update code formatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,50 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const gutil = require('gulp-util');
-const plumber = require('gulp-plumber');
-const chalk = require('chalk');
-const runSequence = require('run-sequence');
-const del = require('del');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const gutil = require("gulp-util");
+const plumber = require("gulp-plumber");
+const chalk = require("chalk");
+const runSequence = require("run-sequence");
+const del = require("del");
 
-const sourceFolder = 'src';
-const distFolder = 'dist';
+const sourceFolder = "src";
+const distFolder = "dist";
 
 const PATH = {
   SRC: {
     APP: sourceFolder,
-    HTML: sourceFolder + '/*.html',
-    ASSETS: sourceFolder + '/assets/*',
-    STYLE: sourceFolder + '/style/*.scss'
+    HTML: sourceFolder + "/*.html",
+    ASSETS: sourceFolder + "/assets/*",
+    STYLE: sourceFolder + "/style/*.scss",
   },
 
   DIST: {
     APP: distFolder,
-    HTML: distFolder + '/',
-    ASSETS: distFolder + '/assets/',
-    STYLE: distFolder + '/style/'
-  }
+    HTML: distFolder + "/",
+    ASSETS: distFolder + "/assets/",
+    STYLE: distFolder + "/style/",
+  },
 };
 
-gulp.task('clean', function () {
+gulp.task("clean", function () {
   return del([PATH.DIST.APP]);
 });
 
-gulp.task('html', function () {
-  return gulp.src(PATH.SRC.HTML)
-    .pipe(gulp.dest(PATH.DIST.HTML));
+gulp.task("html", function () {
+  return gulp.src(PATH.SRC.HTML).pipe(gulp.dest(PATH.DIST.HTML));
 });
 
-gulp.task('assets', function () {
-  return gulp.src(PATH.SRC.ASSETS)
-    .pipe(gulp.dest(PATH.DIST.ASSETS));
+gulp.task("assets", function () {
+  return gulp.src(PATH.SRC.ASSETS).pipe(gulp.dest(PATH.DIST.ASSETS));
 });
 
-gulp.task('sass', function () {
-  return gulp.src(PATH.SRC.STYLE)
-    .pipe(plumber({
-      errorHandler: reportError
-    }))
+gulp.task("sass", function () {
+  return gulp
+    .src(PATH.SRC.STYLE)
+    .pipe(
+      plumber({
+        errorHandler: reportError,
+      })
+    )
     .pipe(sass())
     .pipe(gulp.dest(PATH.DIST.STYLE));
 });
@@ -51,36 +52,31 @@ gulp.task('sass', function () {
 var reportError = function (error) {
   gutil.beep();
 
-  var report = '',
+  var report = "",
     chalk = gutil.colors.white.bgRed;
 
-  report += chalk('TASK:') + ' [' + error.plugin + ']\n';
-  report += chalk('PROB:') + ' ' + error.message + '\n';
+  report += chalk("TASK:") + " [" + error.plugin + "]\n";
+  report += chalk("PROB:") + " " + error.message + "\n";
 
   if (error.lineNumber) {
-    report += chalk('LINE:') + ' ' + error.lineNumber + '\n';
+    report += chalk("LINE:") + " " + error.lineNumber + "\n";
   }
 
   if (error.fileName) {
-    report += chalk('FILE:') + ' ' + error.fileName + '\n';
+    report += chalk("FILE:") + " " + error.fileName + "\n";
   }
 
   console.error(report);
 
-  this.emit('end');
+  this.emit("end");
 };
 
-gulp.task('watch', function () {
-  gulp.watch(PATH.SRC.HTML, ['html']);
-  gulp.watch(PATH.SRC.ASSETS, ['assets']);
-  gulp.watch(PATH.SRC.STYLE, ['sass']);
+gulp.task("watch", function () {
+  gulp.watch(PATH.SRC.HTML, ["html"]);
+  gulp.watch(PATH.SRC.ASSETS, ["assets"]);
+  gulp.watch(PATH.SRC.STYLE, ["sass"]);
 });
 
-gulp.task('default', function () {
-  runSequence(
-    'clean',
-    'html',
-    'assets',
-    'sass'
-  );
+gulp.task("default", function () {
+  runSequence("clean", "html", "assets", "sass");
 });
